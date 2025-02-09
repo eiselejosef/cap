a13d9a0a7678e8072f795af77afdcc94126aa563
Josef Eisele <josef@josefmacbookair.home>
2025-02-09 13:06:19 +0100

    changes from 08.02. with new text for appartment, colour and text booking

--- scripts.js
+++ scripts.js
@@ -14,17 +14,36 @@ document.addEventListener('DOMContentLoaded', function() {
 const translations = {
     en: {
         navbarBrand: "Holiday Apartment",
-        title: "Welcome to Our Holiday Apartment in France",
+        title: "Welcome to Cap Esterel",
         galleryTitle: "Gallery",
         infoTitle: "About the Apartment",
-        infoText: "Our cozy apartment is located in the heart of France, offering a perfect getaway for families and couples. It features a spacious living room, a fully equipped kitchen, and a comfortable bedroom.",
+        infoText: `
+            Spend an unforgettable holiday on the French Riviera in this lovely 2-room apartment of 28m2 with 4 beds, on the 2nd and top floor with an 8m2 terrace. Enjoy the southeast exposure and the magnificent sea view!<br><br>
+            <strong>Layout:</strong><br>
+            - a bedroom with a double bed (140 x 190 cm) + television, access to the terrace<br>
+            - a living room with a sofa bed for 2 people (80 x 190 cm) + television, access to the terrace<br>
+            - equipped kitchen: microwave, dishwasher, coffee machine<br>
+            - bathroom<br>
+            - separate toilet<br>
+            - accommodation for max. 4 people<br><br>
+            <strong>Weekly rental in high season.</strong><br>
+            <strong>INCLUDED IN THE RENTAL PRICE:</strong><br>
+            * CLEANING<br>
+            * ACCESS TO THE POOLS<br>
+            * COVERED PARKING<br><br>
+            <strong>FOR AN ADDITIONAL FEE:</strong><br>
+            BED LINEN AND TOWELS on request.<br>
+            INTERNET/WIFI, personalized welcome package and check-in/check-out.<br>
+            <strong>Deposit:</strong> 600 € per stay<br><br>
+            <em>Please note:</em> Apartment access without elevator. No pets allowed, non-smoking apartment.
+        `,
         servicesTitle: "Services",
         servicesText: "We offer a range of services to make your stay more comfortable.",
         bookingTitle: "How to Book",
-        bookingText: "To book our apartment, please contact us <a href=\"https://www.leboncoin.fr/ad/locations_saisonnieres/2326754619\">leboncoin</a>",
+        bookingText: "To book our apartment or obtain additional information, please contact us via the platform <a href=\"https://www.leboncoin.fr/ad/locations_saisonnieres/2326754619\">leboncoin</a>",
         navHome: "Home",
         navGallery: "Gallery",
-        navAbout: "About",
+        navAbout: "Apartment",
         navServices: "Services",
         navBooking: "Booking",
         navContact: "Contact Us",
@@ -33,18 +52,37 @@ const translations = {
         openGalleryButton: "Open Gallery"
     },
     fr: {
-        navbarBrand: "Appartement de Vacances",
-        title: "Bienvenue dans notre appartement de vacances en France",
+        navbarBrand: "Appartement de vacances",
+        title: "Bienvenue à Cap Esterel",
         galleryTitle: "Galerie",
         infoTitle: "À propos de l'appartement",
-        infoText: "Notre appartement confortable est situé au cœur de la France, offrant une escapade parfaite pour les familles et les couples. Il dispose d'un salon spacieux, d'une cuisine entièrement équipée et d'une chambre confortable.",
+        infoText: `
+            Venez passer des vacances inoubliables sur la Côte d'Azur dans ce joli 2 pièces de 28m2 avec 4 couchages, au 2ème et dernier étage et terrasse de 8m2. Profitez de l'exposition sud-est et de la magnifique vue mer!<br><br>
+            <strong>Aménagement :</strong><br>
+            - une chambre avec lit double (140 x 190 cm) + télévision, accès terrasse<br>
+            - un séjour avec canapé-lit 2 couchages (80 x 190 cm) + télévision, accès terrasse<br>
+            - cuisine équipée : micro-ondes, lave-vaisselle, cafetière<br>
+            - salle-de-bains<br>
+            - toilettes séparées<br>
+            - logement pour 4 pers. max.<br><br>
+            <strong>Location à la semaine en haute saison.</strong><br>
+            <strong>INCLU DANS LE PRIX DE LA LOCATION :</strong><br>
+            * MENAGE<br>
+            * ACCES AUX PISCINES<br>
+            * PARKING COUVERT<br><br>
+            <strong>EN SUPPLEMENT :</strong><br>
+            LINGE DE LITS ET SERVIETTES sur demande.<br>
+            INTERNET/WIFI, forfait Accueil personnalisé et états des lieux d'entrée et de sortie.<br>
+            <strong>Caution :</strong> 600 € par séjour<br><br>
+            <em>Veuillez noter:</em> Accès appartement sans ascenseur. Animaux non admis, appartement non-fumeur.
+        `,
         servicesTitle: "Services",
         servicesText: "Nous offrons une gamme de services pour rendre votre séjour plus confortable.",
         bookingTitle: "Comment réserver",
-        bookingText: "Pour réserver notre appartement, veuillez nous contacter à <a href=\"https://www.leboncoin.fr/ad/locations_saisonnieres/2326754619\">leboncoin</a>",
+        bookingText: "Pour réserver notre appartement ou obtenir des informations supplémentaires, veuillez nous contacter via la plateforme <a href=\"https://www.leboncoin.fr/ad/locations_saisonnieres/2326754619\">leboncoin</a>",
         navHome: "Accueil",
         navGallery: "Galerie",
-        navAbout: "À propos",
+        navAbout: "Appartement",
         navServices: "Services",
         navBooking: "Réservation",
         navContact: "Contactez-nous",
@@ -54,17 +92,36 @@ const translations = {
     },
     it: {
         navbarBrand: "Appartamento per Vacanze",
-        title: "Benvenuti nel nostro appartamento per vacanze in Francia",
+        title: "Benvenuti a Cap Esterel",
         galleryTitle: "Galleria",
         infoTitle: "Informazioni sull'appartamento",
-        infoText: "Il nostro accogliente appartamento si trova nel cuore della Francia, offrendo una fuga perfetta per famiglie e coppie. Dispone di un ampio soggiorno, una cucina completamente attrezzata e una camera da letto confortevole.",
+        infoText: `
+            Venite a trascorrere una vacanza indimenticabile sulla Costa Azzurra in questo grazioso bilocale di 28m2 con 4 posti letto, al 2° e ultimo piano e terrazza di 8m2. Godetevi l'esposizione a sud-est e la magnifica vista mare!<br><br>
+            <strong>Aménagement :</strong><br>
+            - una camera con letto matrimoniale (140 x 190 cm) + televisione, accesso alla terrazza<br>
+            - un soggiorno con divano letto per 2 persone (80 x 190 cm) + televisione, accesso alla terrazza<br>
+            - cucina attrezzata: microonde, lavastoviglie, macchina da caffè<br>
+            - bagno<br>
+            - WC separato<br>
+            - Alloggio per max. 4 persone<br><br>
+            <strong>Affitto settimanale in alta stagione.</strong><br>
+            <strong>INCLUSO NEL PREZZO DELL'AFFITTO:</strong><br>
+            * PULIZIA<br>
+            * ACCESSO ALLE PISCINE<br>
+            * PARCHEGGIO COPERTO<br><br>
+            <strong>A PAGAMENTO:</strong><br>
+            BIANCHERIA DA LETTO E ASCIUGAMANI su richiesta.<br>
+            INTERNET/WIFI, forfait Accoglienza personalizzata e stato dei luoghi all'entrata e all'uscita.<br>
+            <strong>Cauzione:</strong> 600 € per soggiorno<br><br>
+            <em>Si prega di notare:</em> Accesso all'appartamento senza ascensore. Animali non ammessi, appartamento non fumatori.
+        `,
         servicesTitle: "Servizi",
         servicesText: "Offriamo una gamma di servizi per rendere il vostro soggiorno più confortevole, tra cui Wi-Fi gratuito.",
         bookingTitle: "Come prenotare",
-        bookingText: "Per prenotare il nostro appartamento, contattaci a <a href=\"https://www.leboncoin.fr/ad/locations_saisonnieres/2326754619\">leboncoin</a>",
+        bookingText: "Per prenotare il nostro appartamento o ottenere ulteriori informazioni, contattaci tramite la piattaforma <a href=\"https://www.leboncoin.fr/ad/locations_saisonnieres/2326754619\">leboncoin</a>",
         navHome: "Home",
         navGallery: "Galleria",
-        navAbout: "Informazioni",
+        navAbout: "Appartamento",
         navServices: "Servizi",
         navBooking: "Prenotazione",
         navContact: "Contattaci",
@@ -74,17 +131,36 @@ const translations = {
     },
     de: {
         navbarBrand: "Ferienwohnung",
-        title: "Willkommen in unserer Ferienwohnung in Frankreich",
+        title: "Willkommen in Cap Esterel",
         galleryTitle: "Galerie",
         infoTitle: "Über die Wohnung",
-        infoText: "Unsere gemütliche Wohnung befindet sich im Herzen Frankreichs und bietet einen perfekten Rückzugsort für Familien und Paare. Sie verfügt über ein geräumiges Wohnzimmer, eine voll ausgestattete Küche und ein komfortables Schlafzimmer.",
+        infoText: `
+            Verbringen Sie einen unvergesslichen Urlaub an der Côte d'Azur in dieser hübschen 2-Zimmer-Wohnung mit 28m2 und 4 Schlafplätzen, im 2. und letzten Stockwerk und einer 8m2 großen Terrasse. Genießen Sie die südöstliche Ausrichtung und den herrlichen Meerblick!<br><br>
+            <strong>Ausstattung:</strong><br>
+            - ein Schlafzimmer mit Doppelbett (140 x 190 cm) + Fernseher, Zugang zur Terrasse<br>
+            - ein Wohnzimmer mit Schlafsofa für 2 Personen (80 x 190 cm) + Fernseher, Zugang zur Terrasse<br>
+            - ausgestattete Küche: Mikrowelle, Geschirrspüler, Kaffeemaschine<br>
+            - Badezimmer<br>
+            - separates WC<br>
+            - Unterkunft für max. 4 Personen<br><br>
+            <strong>Wochenweise Vermietung in der Hochsaison.</strong><br>
+            <strong>IM MIETPREIS ENTHALTEN:</strong><br>
+            * REINIGUNG<br>
+            * ZUGANG ZU DEN SCHWIMMBÄDERN<br>
+            * ÜBERDACHTER PARKPLATZ<br><br>
+            <strong>GEGEN AUFPREIS:</strong><br>
+            BETTWÄSCHE UND HANDTÜCHER auf Anfrage.<br>
+            INTERNET/WLAN, Pauschale für persönlichen Empfang und Abnahme bei An- und Abreise.<br>
+            <strong>Kaution:</strong> 600 € pro Aufenthalt<br><br>
+            <em>Bitte beachten:</em> Zugang zur Wohnung ohne Aufzug. Haustiere nicht erlaubt, Nichtraucherwohnung.
+        `,
         servicesTitle: "Dienstleistungen",
         servicesText: "Wir bieten eine Reihe von Dienstleistungen, um Ihren Aufenthalt angenehmer zu gestalten.",
         bookingTitle: "Wie man bucht",
-        bookingText: "Um unsere Wohnung zu buchen, kontaktieren Sie uns bitte unter <a href=\"https://www.leboncoin.fr/ad/locations_saisonnieres/2326754619\">leboncoin</a>",
+        bookingText: "Um unsere Wohnung zu buchen oder weitere Informationen zu erhalten, kontaktieren Sie uns bitte über die Plattform <a href=\"https://www.leboncoin.fr/ad/locations_saisonnieres/2326754619\">leboncoin</a>",
         navHome: "Startseite",
         navGallery: "Galerie",
-        navAbout: "Über uns",
+        navAbout: "Ferienwohnung",
         navServices: "Dienstleistungen",
         navBooking: "Buchung",
         navContact: "Kontaktieren Sie uns",
@@ -99,7 +175,7 @@ function setLanguage(language) {
     document.getElementById('title').innerText = translations[language].title;
     document.getElementById('gallery-title').innerText = translations[language].galleryTitle;
     document.getElementById('info-title').innerText = translations[language].infoTitle;
-    document.getElementById('info-text').innerText = translations[language].infoText;
+    document.getElementById('info-text').innerHTML = translations[language].infoText;
     document.getElementById('services-title').innerText = translations[language].servicesTitle;
     document.getElementById('services-text').innerText = translations[language].servicesText;
     document.getElementById('booking-title').innerText = translations[language].bookingTitle;
